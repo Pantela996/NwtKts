f1a1fea10ed11f982dfefc7a433ab6b24c682e0e
milos.pantic <milos.pantic@D130025.dms.local>
2019-11-07 16:10:43 +0100

    security changes

---
+++ master/src/main/java/com/project/controller/LoginController.java
@@ -0,0 +1,19 @@
+package com.project.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LoginController {
+	
+	@RequestMapping(value = "/login", method = RequestMethod.POST)
+	public String login() {
+		return "Success login";
+	}
+}
+	
